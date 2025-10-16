8d3bd68d59ec485691b9ee51b1d6b7861b037988
Rebuild Robotics <162683187+RebuildRobotics@users.noreply.github.com>
2025-10-16 10:47:14 +0300

    Add files via upload

--- CombatBot_BeetleESP32C3_BLE_Gamepad_V.1.4.2.ino
+++ CombatBot_BeetleESP32C3_BLE_Gamepad_V.1.4.2.ino
@@ -240,7 +240,11 @@ String version = "1.4.2";
   Script builders and component manufacturers are not responsiple from possible damages.
   Third party script writers hasn't got nothing to do with this project except I'm using their libraries.
 
-  Copyright (C) Ville Ollila (RoboticsIsForNerds, Rebuild Robotics). Licensed under MIT license.
+  Copyright (C) Ville Ollila (RoboticsIsForNerds, Rebuild Robotics).
+  
+  This work is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License.
+  To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/4.0/
+
   Examples used are linked in functions.
 
   Third party copyrights:
